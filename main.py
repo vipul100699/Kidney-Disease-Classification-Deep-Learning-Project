43a2efba046d58cda611f86df4c43f2f6f66d51c
Vipul Goel <vipulgoel1091@gmail.com>
2025-09-02 18:24:41 +0530

    Preparing the BASE MODEL implementation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -8,6 +9,19 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<< \n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model Stage"
+
+try:
+        logger.info(f'**************')
+        logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<')
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f'>>>>>> stage {STAGE_NAME} completed <<<<<<')
+
 except Exception as e:
     logger.exception(e)
     raise e
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,10 +1,10 @@
 from cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig
 
 
 class ConfigurationManager:
-    def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=CONFIG_FILE_PATH):
+    def __init__(self, config_filepath=CONFIG_FILE_PATH, params_filepath=PARAMS_FILE_PATH):
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
 
@@ -23,3 +23,25 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+
+        create_directories([config.root_dir])
+
+        print(f"Keys in self.params: {list(self.params.keys())}")
+        print(f"self.params content: {self.params}")
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
